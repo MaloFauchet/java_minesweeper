cf76ec747f4c6b6c63cbca80e844320ce3f750b8
FAUCHET Malo <malofauchetpro@gmail.com>
2023-10-29 10:46:45 +0100

    added plantMine method

--- src/Algo.java
+++ src/Algo.java
@@ -1,30 +1,56 @@
 public class Algo {
+    final char EMPTY_CELL = '_';
+
     int[] size;  // size of the grid. first is num of rows. second is num of columns
     int mine_number;  // number of mines
-    int[][] grid;
+    int[][] algo_grid;  // the algo grid. contains mine positions and the numbers of mines next to a case
+    int[][] game_grid;  // the game grid. the one printed and updated in function of user input
 
 
     public Algo(int[] grid_size, int p_mine_number){
         size = grid_size;
         mine_number = p_mine_number;
-        grid = new int[size[0]][size[1]];
+        algo_grid = new int[size[0]][size[1]];
+        game_grid = new int[size[0]][size[1]];
 
         initGrid();
+        plantMine();
         printGrid();
     }
 
     public void initGrid() {
         for (int i = 0; i < size[0]; i++) {
             for (int j = 0; j < size[1]; j++) {
-                grid[i][j] = 0;
+                algo_grid[i][j] = 0;
+            }
+        }
+    }
+
+    public void plantMine() {
+        int mine_planted = 0;
+        int x, y;
+
+        while (mine_planted < mine_number) {
+            x = getRandomInt(size[1]);
+            y = getRandomInt(size[0]);
+            if (algo_grid[y][x] != -1) {
+                algo_grid[y][x] = -1;
+                mine_planted++;
             }
         }
     }
 
     public void printGrid() {
         for (int i = 0; i < size[0]; i++) {
+            if (i==0) {
+                System.out.println("   |  0. 1. 2. 3. 4. 5. 6. 7. 8. 9.");
+                System.out.println("___|_______________________________");
+            }
             for (int j = 0; j < size[1]; j++) {
-                System.out.format("%4d", grid[i][j]);
+                if (j == 0) {
+                    System.out.print(i+". |");
+                }
+                System.out.format("%3d", algo_grid[i][j]);
             }
             System.out.println();
         }
